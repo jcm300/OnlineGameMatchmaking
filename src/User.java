271d7999f4bd67214695435abbed993dfd888b2e
MiguelQ <miguelquaresma97@gmail.com>
2017-12-07 23:43:20 +0000

    Initial user implementation added

---
+++ src/User.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author miguelq
+ */
+public class User {
+    private String uName;
+    private String email;
+    private String password;
+    private int rank;
+    
+    public User(String name, String uEmail, String pass){
+        this.uName=name;
+        this.email=uEmail;
+        this.password=pass;
+        this.rank=0;
+    }
+}
