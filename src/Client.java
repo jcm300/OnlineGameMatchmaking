1a993c1c6b18926b9c5e295b25ebbae72476a5e3
jcm300 <jcm300@live.com.pt>
2017-12-09 23:06:38 +0000

    authenticate method in beta

--- src/Client.java
+++ src/Client.java
@@ -1,4 +1,21 @@
 //Class that each gamer will execute
 public class Client {
+    private String username;
 
+    private Socket authenticate(String username, String password){
+        Socket s = new Socket("127.0.0.1",9999);
+
+        PrintWriter out = new PrintWriter(s.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+        
+        String current;
+        out.println(username + ";" + password);
+
+        while((current = in.readLine()) != null){
+            in.close();
+            out.close();
+            if(current.equals("UserNotFound")) s.close();
+        }
+        return s;
+    }
 }
