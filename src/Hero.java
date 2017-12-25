ba93b37fddc5475f859ac9739b0666c36c12d8ce
jcm300 <zeclmartins@gmail.com>
2017-12-25 17:38:01 +0000

    added get/set methods to Hero class

--- src/Hero.java
+++ src/Hero.java
@@ -6,4 +6,20 @@ public class Hero{
         this.id = id;
         this.name = n;
     }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public void setName(String name){
+        this.name=name;
+    }
 }
